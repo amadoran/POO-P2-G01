2af32d5908f15ec8fd0f5ec8526c0e73b5a6bde6
jegaibor <107864965+Jorge2033@users.noreply.github.com>
2022-07-15 08:16:20 +0000

    Implementación de ciertos métodos en las clases Cliente y cita, no de manera completa

--- modelo/Cita.java
+++ modelo/Cita.java
@@ -1,5 +1,40 @@
 package modelo;
+import java.util.*;
 public class Cita{
   private String fechacita;
   private String tipoCita;
+  public Cita(String fechacita,String tipoCita){
+    this.fechacita=fechacita;
+    this.tipoCita=tipoCita;
+    
+  }
+  public String crearCita(String fechacita,String tipoCita){
+    new Cita(fechacita,tipoCita);
+    return "Cita creada:"+fechacita+""+tipoCita;
+    
+  }
+   public String consultarCita(String fechacita,String tipoCita){
+    Scanner usuario2=new Scanner(System.in);
+     System.out.println("Ingrese la fecha de la cita:");
+     String fechausuario=usuario2.next();
+     if(fechausuario.equals(fechacita)){
+      
+     }
+     return "Consulta de la cita";
+  }
+  public void eliminarCita(){
+    Scanner usuario=new Scanner(System.in);
+    System.out.println("Ingrese la cedula del usuario");
+    int Cedulaeliminar=usuario.nextInt();
+    if(){}
+    
+    
+  }
+    public String getFechaCita(){
+    return fechacita;
+  } 
+  public String getTipoCita(){
+    return tipoCita;
+  }
+ 
 }
--- modelo/Cliente.java
+++ modelo/Cliente.java
@@ -1,14 +1,22 @@
 package modelo;
 public class Cliente extends Personal{
   private String representante;
-  public Cliente(int cedula, String nombre, String apellido,int telefono, String correo, String representante){
+  public Cliente( int cedula,String nombre, String apellido,int telefono, String correo, String representante){
     super(cedula,nombre,apellido,telefono,correo);
     this.representante=representante;
+  } 
+  public Cliente( String nombre, String apellido,int telefono, String correo, String representante){
+    super(nombre,apellido,telefono,correo);
+    this.representante=representante;
   }
-  public void agregarCliente(int cedula, String nombre, String apellido, int telefono, String correo,String representante){
-    
+  public static void agregarCliente( int cedula,String nombre, String apellido, int telefono, String correo,String representante){
+    new Cliente(cedula,nombre,apellido,telefono,correo,representante);
+    System.out.println("Cliente agregado");
   }
-  public void editarCliente(int cedula, String nombre, String apellido, int telefono, String correo, String representante){
-    
+  public static void editarCliente( String nombre, String apellido, int telefono, String correo, String representante){
+     if (!(new Cliente(nombre,apellido,telefono,correo,representante).equals())){
+      
+      
+    }
   }
 }
