447f8164d638c6fc5b538026175dcb3bd033fcca
Andrés Amador <107083400+amadoran@users.noreply.github.com>
2022-07-17 23:51:16 +0000

    inicializarSistema

--- Main.java
+++ Main.java
@@ -6,14 +6,27 @@ import java.util.Scanner;
 public class Main{
   public static ArrayList<Servicio> servicios;
   public static ArrayList<Cita> citas;
-  public static ArrayList<Cliente> clientes;
-  public static ArrayList<Empleado> empleados;
   public static ArrayList<Atencion> atenciones;
+  public static ArrayList<Personal> personas;
   
   public static void inicializarSistema(){
+    Representante rep1 = new Representante(901555730, "Esteban", "Gaibor", 918810059, "estebangaibor@gmail.com");
+    Representante rep2 = new Representante(954680245, "Maria", "Diaz", 935153129, "mariadiaz@gmail.com");
     servicios = new ArrayList<>();
+    personas = new ArrayList<>();
+    citas = new ArrayList<>();
+    atenciones = new ArrayList<>();
     servicios.add(new Servicio("Terapia de Lenguaje", 45, 60.0f, Estado.ACTIVO));
-  }  
+    servicios.add(new Servicio("Terapia Psicopedagógica", 45, 60.0f, Estado.ACTIVO));
+    servicios.add(new Servicio("Terapia Cognitiva", 45, 60.0f, Estado.ACTIVO));
+    personas.add(new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO));
+    personas.add(new Cliente(932764534, "Jaime", "Gaibor", 974874878, "jaimegaibor@gmail.com",rep1));
+    personas.add(new Cliente(958625741, "Antonio", "Amador", 997353370, "antonioamador@gmail.com",rep2));
+    citas.add(new Cita("20/10/2022", servicios.get(0), new Cliente(932764534, "Jaime", "Gaibor", 974874878, "jaimegaibor@gmail.com",rep1), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO)));
+    citas.add(new Cita("25/10/2022", servicios.get(1), new Cliente(958625741, "Antonio", "Amador", 997353370, "antonioamador@gmail.com",rep2), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO)));
+    atenciones.add(new Atencion(new Cita("17/10/2022", servicios.get(2), new Cliente(932764534, "Jaime", "Gaibor", 974874878, "jaimegaibor@gmail.com",rep1), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO)), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO), 30));
+  } 
+    
 
   Scanner input = new Scanner(System.in;
   System.out.println("---MENU---");
--- modelo/Cliente.java
+++ modelo/Cliente.java
@@ -1,15 +1,15 @@
 package modelo;
 public class Cliente extends Personal{
-  private String representante;
-  public Cliente( int cedula,String nombre, String apellido,int telefono, String correo, String representante){
+  private Representante representante;
+  public Cliente( int cedula,String nombre, String apellido,int telefono, String correo, Representante representante){
     super(cedula,nombre,apellido,telefono,correo);
     this.representante=representante;
   } 
-  public static void agregarCliente( int cedula,String nombre, String apellido, int telefono, String correo,String representante){
+  public static void agregarCliente( int cedula,String nombre, String apellido, int telefono, String correo,Representante representante){
     new Cliente(cedula,nombre,apellido,telefono,correo,representante);
     System.out.println("Cliente agregado");
   }
-  public void editarCliente( String nombre, String apellido, int telefono, String correo, String representante){
+  public void editarCliente( String nombre, String apellido, int telefono, String correo, Representante representante){
     setNombre(nombre);
     setApellido(apellido);
     setTelefono(telefono);
