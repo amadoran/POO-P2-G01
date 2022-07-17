9427fc3d6b10b8f606db46315ed0dbf79c0d05ab
Andrés Amador <107083400+amadoran@users.noreply.github.com>
2022-07-17 23:55:45 +0000

    Correccion main

--- Main.java
+++ Main.java
@@ -27,34 +27,27 @@ public class Main{
     atenciones.add(new Atencion(new Cita("17/10/2022", servicios.get(2), new Cliente(932764534, "Jaime", "Gaibor", 974874878, "jaimegaibor@gmail.com",rep1), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO)), new Empleado(958024996, "Pedro", "Flores", 991056746, "pedroflores@gmail.com", Estado.ACTIVO), 30));
   } 
     
-
-  Scanner input = new Scanner(System.in;
-  System.out.println("---MENU---");
-  System.out.println("1.Servicios\n2.Empleados\n3.Clientes\n4.Citas\n5.Atencion\n6.Salir\nElija la opcion que desee:");
-
-int opcion= input.nextInt();
-switch(opcion){
-  case 1:
-    System.out.println(servicios);
-    break;
-  case 2:
-    Empleado.cuerpo();
-    break;
-  case 3:
-    Cliente.cuerpo();
-    break;
-  case 4:
-    break;
-  case 5:
-    Atencion.cuerpo();
-  case 6:
-    break;
-}
-
-
-  
   public static void main(String[] args){
-    System.out.println("Hello World");
-    inicializarSistema();
-  }  
+    Scanner input = new Scanner(System.in);
+    System.out.println("---MENU---");
+    System.out.println("1.Servicios\n2.Empleados\n3.Clientes\n4.Citas\n5.Atencion\n6.Salir\nElija la opcion que desee:");
+    int opcion= input.nextInt();
+    switch(opcion){
+      case 1:
+        System.out.println(servicios);
+        break;
+      case 2:
+        Empleado.cuerpo();
+        break;
+      case 3:
+        Cliente.cuerpo();
+        break;
+      case 4:
+        break;
+      case 5:
+        Atencion.cuerpo();
+      case 6:
+        break;
+    }
+  }
 }
