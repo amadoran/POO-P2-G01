08933f9ea1c7ac483e3d09257ab170dfd156ebda
Andrés Amador <107083400+amadoran@users.noreply.github.com>
2022-07-17 22:51:22 +0000

    Base para main

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
+//Clase menú
+import java.util.ArrayList;
+import modelo.*;
 public class Main{
+  public static ArrayList<Servicio> servicios;
+  public static ArrayList<Cita> citas;
+  public static ArrayList<Cliente> clientes;
+  public static ArrayList<Empleado> empleados;
+  public static ArrayList<Atencion> atenciones;
+  
+  public static void inicializarSistema(){
+    servicios = new ArrayList<>();
+    servicios.add(new Servicio("Terapia de Lenguaje", 45, 60.0f, Estado.ACTIVO));
+  }
   public static void main(String[] args){
     System.out.println("Hello World");
   }  
