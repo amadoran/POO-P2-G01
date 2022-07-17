9682fc093379ca44c78bec5cd0cb5973c1ec145e
jegaibor <107864965+Jorge2033@users.noreply.github.com>
2022-07-17 22:31:05 +0000

    Se creo nuevos atributos en la clase cita y se hizo modificaciones en los constructores al igual que en los métodos

--- modelo/Cita.java
+++ modelo/Cita.java
@@ -2,39 +2,50 @@ package modelo;
 import java.util.*;
 public class Cita{
   private String fechacita;
-  private String tipoCita;
-  public Cita(String fechacita,String tipoCita){
+  private Servicio servicio;
+  private Cliente cliente;
+  private Empleado empleado;
+  public Cita(String fechacita,Servicio servicio, Cliente cliente,Empleado empleado){
     this.fechacita=fechacita;
-    this.tipoCita=tipoCita;
-    
+    this.servicio=servicio;
+    this.cliente=cliente;
+    this.empleado=empleado;
   }
-  public String crearCita(String fechacita,String tipoCita){
-    new Cita(fechacita,tipoCita);
-    return "Cita creada:"+fechacita+""+tipoCita;
+  public String crearCita(String fechacita,Servicio servicio,Cliente cliente, Empleado empleado){
+    new Cita(fechacita,servicio,cliente,empleado);
+    return "Cita creada";
     
   }
    public String consultarCita(String fechacita,String tipoCita){
     Scanner usuario2=new Scanner(System.in);
-     System.out.println("Ingrese la fecha de la cita:");
-     String fechausuario=usuario2.next();
-     if(fechausuario.equals(fechacita)){
+     if(fechacita.isEmpty()){
+       System.out.println("No hay citas pendientes");
       
      }
-     return "Consulta de la cita";
+     else{
+       System.out.println("Escriba la fecha de la cita que desea eliminar");
+            System.out.println("Ingrese la fecha de la cita:");
+     String fechausuario=usuario2.nextLine();
+       for (int i=0;i<Cita.size();i++){
+         if(fechausuario.euals(Cita.get(i).getFechaCita()&&
+         
+      }
+     }
   }
   public void eliminarCita(){
     Scanner usuario=new Scanner(System.in);
     System.out.println("Ingrese la cedula del usuario");
     int Cedulaeliminar=usuario.nextInt();
-    if(){}
+    
     
     
   }
     public String getFechaCita(){
     return fechacita;
   } 
-  public String getTipoCita(){
-    return tipoCita;
-  }
- 
+ public Cliente getCliente(){
+  return cliente;}
+public Servicio getServicio(){
+  return servicio;
+}
 }
--- modelo/Cliente.java
+++ modelo/Cliente.java
@@ -5,18 +5,15 @@ public class Cliente extends Personal{
     super(cedula,nombre,apellido,telefono,correo);
     this.representante=representante;
   } 
-  public Cliente( String nombre, String apellido,int telefono, String correo, String representante){
-    super(nombre,apellido,telefono,correo);
-    this.representante=representante;
-  }
   public static void agregarCliente( int cedula,String nombre, String apellido, int telefono, String correo,String representante){
     new Cliente(cedula,nombre,apellido,telefono,correo,representante);
     System.out.println("Cliente agregado");
   }
-  public static void editarCliente( String nombre, String apellido, int telefono, String correo, String representante){
-     if (!(new Cliente(nombre,apellido,telefono,correo,representante).equals())){
-      
-      
+  public void editarCliente( String nombre, String apellido, int telefono, String correo, String representante){
+    setNombre(nombre);
+    setApellido(apellido);
+    setTelefono(telefono);
+    setCorreo(correo);
+    this.representante = representante;
     }
   }
-}
