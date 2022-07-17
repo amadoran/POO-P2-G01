d3cc91b8cecc985568bfe963f4db84a3a5fe2d8d
Guido Flores <107821723+GuidoFlores@users.noreply.github.com>
2022-07-17 23:18:11 +0000

    clase representante menu

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 //Clase menú
 import java.util.ArrayList;
 import modelo.*;
+import java.util.Scanner;
+
 public class Main{
   public static ArrayList<Servicio> servicios;
   public static ArrayList<Cita> citas;
@@ -11,8 +13,35 @@ public class Main{
   public static void inicializarSistema(){
     servicios = new ArrayList<>();
     servicios.add(new Servicio("Terapia de Lenguaje", 45, 60.0f, Estado.ACTIVO));
-  }
+  }  
+
+  Scanner input = new Scanner(System.in;
+  System.out.println("---MENU---");
+  System.out.println("1.Servicios\n2.Empleados\n3.Clientes\n4.Citas\n5.Atencion\n6.Salir\nElija la opcion que desee:");
+
+int opcion= input.nextInt();
+switch(opcion){
+  case 1:
+    System.out.println(servicios);
+    break;
+  case 2:
+    Empleado.cuerpo();
+    break;
+  case 3:
+    Cliente.cuerpo();
+    break;
+  case 4:
+    break;
+  case 5:
+    Atencion.cuerpo();
+  case 6:
+    break;
+}
+
+
+  
   public static void main(String[] args){
     System.out.println("Hello World");
+    inicializarSistema();
   }  
 }
---
+++ modelo/Representante.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Representante extends Personal{
+  public Representante(int cedula, String nombre, String apellido, int telefono, String correo){
+super(cedula,nombre,apellido,telefono,correo);
+  }
+}
