75d179196f2b68f28d63eefffe74da5bb283b4ee
afamador <afamador@espol.edu.ec>
2022-08-14 18:46:44 -0500

    Creacion del menú y funcionalidad del boton salir

---
+++ src/main/java/ec/espol/poop2g1/MenuController.java
@@ -0,0 +1,36 @@
+package ec.espol.poop2g1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MenuController {
+    @FXML
+    private AnchorPane scene;
+    @FXML
+    private Button servicios;
+    @FXML
+    private Button empleados;
+    @FXML
+    private Button atenciones;
+    @FXML
+    private Button citas;
+    @FXML
+    private Button clientes;
+    @FXML
+    private Button salirBtn;
+
+    public void salir(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Alerta");
+        alert.setHeaderText("Va a salir del programa.");
+        alert.setContentText("Click \"OK\" para confirmar.");
+        if (alert.showAndWait().get() == ButtonType.OK){
+            Stage stage = (Stage) scene.getScene().getWindow();
+            stage.close();
+        }
+    }
+}
