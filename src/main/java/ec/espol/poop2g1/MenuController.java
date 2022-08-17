cccfd2234b29bc9b69726117a4cd7f5c6ae8009b
Jorge2033 <jegaibor@espol.edu.ec>
2022-08-16 20:40:56 -0500

    pequeños cambios

--- src/main/java/ec/espol/poop2g1/MenuController.java
+++ src/main/java/ec/espol/poop2g1/MenuController.java
@@ -1,12 +1,21 @@
 package ec.espol.poop2g1;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import modelo.Atencion;
+import modelo.Cita;
+import modelo.Cliente;
+import modelo.Representante;
 
+import javax.swing.*;
+import java.awt.event.*;
 public class MenuController {
     @FXML
     private AnchorPane scene;
@@ -23,14 +32,17 @@ public class MenuController {
     @FXML
     private Button salirBtn;
 
-    public void salir(){
+    public void salir() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Alerta");
         alert.setHeaderText("Va a salir del programa.");
         alert.setContentText("Click \"OK\" para confirmar.");
-        if (alert.showAndWait().get() == ButtonType.OK){
+        if (alert.showAndWait().get() == ButtonType.OK) {
             Stage stage = (Stage) scene.getScene().getWindow();
             stage.close();
         }
     }
+
 }
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ec.espol.poop2g1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens ec.espol.poop2g1 to javafx.fxml;
