8e7c0addd16ea407e19851ae13092dcacbf7a4bd
Guido Flores <gjflores@espol.edu.ec>
2022-08-24 23:56:53 -0500

    intento de insertar imagenes

--- src/main/java/ec/espol/poop2g01/controladores/JuegoController.java
+++ src/main/java/ec/espol/poop2g01/controladores/JuegoController.java
@@ -1,4 +1,23 @@
 package ec.espol.poop2g01.controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.*;
+
+
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Stack;
+
 public class JuegoController {
+
+
+    public void InsertarImagenFlecha() throws Exception{
+
+        FileInputStream input = new FileInputStream("ec.espol.poop2g01/imagenes/derecha.png");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+
+
+    }
 }
